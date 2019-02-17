b6da8ef1abc779c4c390e489e4a97e3052be1d4c
Talal Alhelo <talal.helo@gmail.com>
2019-02-17 08:49:20 -0800

    Unit test

---
+++ WebAPITests/Controllers/FeedsControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class FeedsControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            var controller = new FeedsController();
+            var result = controller.Get(new Models.RequestQueryParams() {
+                q = "159968055@N03"
+            });
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
